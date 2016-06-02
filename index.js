ea6223742bc3dc86ef2eaada3c9c3b36a182a418
Olya <olka_krishtal@mail.ru>
2016-06-02 10:13:55 +0000

    :done

---
+++ index.js
@@ -0,0 +1,16 @@
+function innerFunc (){
+    function fibonacci(n) {
+        var a = 1;
+        var b = 1;
+        for (var i = 3; i <= n; i++) {
+            var c = a + b;
+            a = b;
+            b = c;
+        }         
+        return "Number Fibonacci = " + b;
+    }
+    var numArray = prompt("Enter the value of the array");
+    var message = fibonacci(numArray);
+    alert(message);
+}
+fibonacci.addEventListener("click", innerFunc);
